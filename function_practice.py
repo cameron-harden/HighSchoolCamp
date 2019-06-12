5c08f4bca076a5c3929c0958c83f7bb4506a5f43
Cameron Harden <cameron.harden109@gmail.com>
2019-06-12 11:47:21 -0500

    Adding function exercises

---
+++ function_practice.py
@@ -0,0 +1,16 @@
+"""
+title: function_practice
+author: Cameron
+date: 2019-06-12 11:22
+"""
+
+
+def age_calculator(year1, year2):
+    return abs(year1-year2)
+
+def avg_numbers(num1, num2, num3):
+    return (num1 + num2 + num3) / 3
+
+print(age_calculator(2018, 1900))
+print(avg_numbers(4, 7, 8))
+
